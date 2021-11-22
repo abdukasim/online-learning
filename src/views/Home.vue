a51fac22cf7c12d34e10377cf300ce9562bd8ef7
kimoAK1 <kimorock59@gmail.com>
2021-11-22 11:20:36 +0300

    Project Setup
    Removed default vue boilerplate, added tailwindcss and modified homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div
+    class="
+      my-0
+      mx-auto
+      min-h-screen
+      flex
+      justify-center
+      items-center
+      text-center
+    "
+  >
+    <div>
+      <img src="@/assets/images/cape.png" class="logo" alt="" />
+
+      <h2 class="subtitle font-light mt-6 text-lg">
+        Temaru Educational Platform
+      </h2>
+      <div class="links">
+        <router-link to="/login" class="button--green"> Login </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style>
+.links {
+  padding-top: 15px;
+}
+</style>
