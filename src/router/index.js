0b1a8f8a4d4fcf607787118f0735aacddecf6857
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-01-10 19:49:25 +0300

    Added student routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+
 import Teacher from "@/views/Teacher/Teacher.vue";
 import Lessons from "@/views/Teacher/Lessons.vue";
-import StudentView from "@/views/Teacher/StudentView.vue";
+import StudentList from "@/views/Teacher/StudentList";
+
+import Student from "@/views/Student/Student.vue";
+import Courses from "@/views/Student/Courses.vue";
+import Profile from "@/views/Student/Profile.vue";
 
 Vue.use(VueRouter);
 
@@ -30,7 +35,22 @@ const routes = [
       },
       {
         path: "/students",
-        component: StudentView,
+        component: StudentList,
+      },
+    ],
+  },
+  {
+    path: "/student",
+    name: "Student",
+    component: Student,
+    children: [
+      {
+        path: "/courses",
+        component: Courses,
+      },
+      {
+        path: "/profile",
+        component: Profile,
       },
     ],
   },
